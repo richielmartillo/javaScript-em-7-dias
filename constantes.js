69559bda21c6c5e22f11604ae144c9dcefd911bc
Richard Marlon Balestrim <richardbalestrim8@gemail.com>
2026-06-24 16:45:49 -0300

    first commit

---
+++ constantes.js
@@ -0,0 +1,9 @@
+const price = 30;
+const productName = 'Curso de JavaScript';
+const discount = 5;
+const finalPrice = price - discount;
+
+console.log('Produto:', productName);
+console.log('Preço:', price);
+console.log('Desconto:', discount);
+console.log('Preço final:', finalPrice);
---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+let firstName = 'Richard Marlon';
+let lastName = 'Balestrim';
+let age = 36;
+let city = 'Maringa';
+let country = 'Brasil';
+let profession = 'QA Engineer Jr';
+let course = 'Engenharia de Qualidade de Software';
+let school = 'EBAC';
+let language1 = 'Portugues';
+let language2 = 'Espanhol';
+let isStudying = true;
+let hasGitHub = true;
+let favoriteTool = 'Vs Code';
+let testTool = 'Cypress';
+let database = 'MongoDB';
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(city);
+console.log(country);
+console.log(profession);
+console.log(course);
+console.log(school);
+console.log(language1);
+console.log(language2);
+console.log(isStudying);
+console.log(hasGitHub);
+console.log(favoriteTool);
+console.log(testTool);
+console.log(database);
